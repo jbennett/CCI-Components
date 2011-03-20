3fd921e3fc7c67b6e618a00325a511fac3758817
Jonathan Bennett <jbennett@ccistudios.com>
2011-03-20 07:53:00 -0400

    Added view sections bar

--- com_gridview/administrator/components/com_gridview/views/html.php
+++ com_gridview/administrator/components/com_gridview/views/html.php
@@ -3,5 +3,14 @@ defined('KOOWA') or die;
 
 class ComGridviewViewHtml extends ComDefaultViewHtml {
 
+	public function __construct(KConfig $config) {
+		$config->views = array(
+			'galleries'	=> JText::_('Galleries'),
+			'samples'	=> JText::_('Samples'),
+			'images'	=> JText::_('Images')
+		);
+
+		parent::__construct($config);
+	}
 }
 ?>
