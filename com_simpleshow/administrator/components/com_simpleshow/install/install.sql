ac650f8d81dc9d2fbc0411d07040e5f3667ebf51
Jonathan Bennett <jbennett@ccistudios.com>
2011-04-13 12:13:08 -0400

    typo

--- com_simpleshow/administrator/components/com_simpleshow/install/install.sql
+++ com_simpleshow/administrator/components/com_simpleshow/install/install.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `#__siimpleshow_images` (
+CREATE TABLE `#__simpleshow_images` (
 	`simpleshow_image_id` SERIAL,
 	`filename` VARCHAR( 250 ) NOT NULL ,
 	`title` VARCHAR( 250 ) NOT NULL,
